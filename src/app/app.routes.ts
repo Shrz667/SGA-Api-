1b6006a564600e172c29cd253476154952689e03
Shrz667 <shyrazguedd.pro@gmail.com>
2025-10-26 22:20:48 +0100

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
- import { HomeComponent } from './pages/home/home.component';// adapte le chemin si besoin
-
-
-export const routes: Routes = [
-   { path: '', component: HomeComponent },   // affiche Home par défaut
-     { path: 'home', component: HomeComponent }  // route vers Home
-
-];
